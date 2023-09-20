4210a99add46a7d008fbb295714ad1ffa5a89f5d
jineeel <iiojineee@gmail.com>
2023-09-20 23:13:06 +0900

    Chore : gitignore 수정

--- src/main/java/com/developer/foodfit/config/oauth/OAuth2UserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.developer.foodfit.config.oauth;
-public interface OAuth2UserInfo {
-
-    String getProviderId();
-    String getProvider();
-    String getEmail();
-    String getName();
-
-}
--- src/main/java/com/developer/foodfit/controller/ItemController.java
+++
@@ -1,7 +0,0 @@
-package com.developer.foodfit.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ItemController {
-}
